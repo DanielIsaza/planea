d73d735b2ceb65d6dcdf665d2899176a6211dc2e
DanielIsaza <danielisaza9@gmail.com>
2017-11-08 23:49:02 -0500

    permisos creados completamente

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -146,6 +146,138 @@ class DatabaseSeeder extends Seeder
           'name'=> 'areas.read',
           'slug' => 'areas.read',
           'description' => 'Ver áreas de conocimiento'
+        ],[
+          'name'=> 'semestres.create',
+          'slug' => 'semestres.create',
+          'description' => 'Crear semestres'
+        ],[
+          'name'=> 'semestres.update',
+          'slug' => 'semestres.update',
+          'description' => 'Actualizar semestres'
+        ],[
+          'name'=> 'semestres.delete',
+          'slug' => 'semestres.delete',
+          'description' => 'Eliminar semestres'
+        ],[
+          'name'=> 'semestres.read',
+          'slug' => 'semestres.read',
+          'description' => 'Ver semestres'
+        ],[
+          'name'=> 'actividadesacademicas.create',
+          'slug' => 'actividadesacademicas.create',
+          'description' => 'Crear actividades académicas'
+        ],[
+          'name'=> 'actividadesacademicas.update',
+          'slug' => 'actividadesacademicas.update',
+          'description' => 'Actualizar actividades académicas'
+        ],[
+          'name'=> 'actividadesacademicas.delete',
+          'slug' => 'actividadesacademicas.delete',
+          'description' => 'Eliminar actividades académicas'
+        ],[
+          'name'=> 'naturalezas.read',
+          'slug' => 'naturalezas.read',
+          'description' => 'Ver naturalezas de los espacios académicos'
+        ],[
+          'name'=> 'naturalezas.create',
+          'slug' => 'naturalezas.create',
+          'description' => 'Crear naturalezas de los espacios académicos'
+        ],[
+          'name'=> 'naturalezas.update',
+          'slug' => 'naturalezas.update',
+          'description' => 'Actualizar naturalezas de los espacios académicos'
+        ],[
+          'name'=> 'naturalezas.delete',
+          'slug' => 'naturalezas.delete',
+          'description' => 'Eliminar naturalezas de los espacios académicos'
+        ],[
+          'name'=> 'naturalezas.read',
+          'slug' => 'naturalezas.read',
+          'description' => 'Ver naturalezas de los espacios académicos'
+        ],[
+          'name'=> 'objetivos.create',
+          'slug' => 'objetivos.create',
+          'description' => 'Crear objetivos de los espacios académicos'
+        ],[
+          'name'=> 'objetivos.update',
+          'slug' => 'objetivos.update',
+          'description' => 'Actualizar objetivos de los espacios académicos'
+        ],[
+          'name'=> 'objetivos.delete',
+          'slug' => 'objetivos.delete',
+          'description' => 'Eliminar objetivos de los espacios académicos'
+        ],[
+          'name'=> 'objetivos.read',
+          'slug' => 'objetivos.read',
+          'description' => 'Ver objetivos de los espacios académicos'
+        ],[
+          'name'=> 'habilidades.create',
+          'slug' => 'habilidades.create',
+          'description' => 'Crear habilidades'
+        ],[
+          'name'=> 'habilidades.update',
+          'slug' => 'habilidades.update',
+          'description' => 'Actualizar habilidades'
+        ],[
+          'name'=> 'habilidades.delete',
+          'slug' => 'habilidades.delete',
+          'description' => 'Eliminar habilidades'
+        ],[
+          'name'=> 'habilidades.read',
+          'slug' => 'habilidades.read',
+          'description' => 'Ver habilidades'
+        ],[
+          'name'=> 'estadisticas.read',
+          'slug' => 'estadisticas.read',
+          'description' => 'Ver Estadísticas'
+        ],[
+          'name'=> 'tipoevaluacion.create',
+          'slug' => 'tipoevaluacion.create',
+          'description' => 'Crear tipo de evaluacion'
+        ],[
+          'name'=> 'tipoevaluacion.update',
+          'slug' => 'tipoevaluacion.update',
+          'description' => 'Actualizar tipo de evaluacion'
+        ],[
+          'name'=> 'tipoevaluacion.delete',
+          'slug' => 'tipoevaluacion.delete',
+          'description' => 'Eliminar tipo de evaluacion'
+        ],[
+          'name'=> 'tipoevaluacion.read',
+          'slug' => 'tipoevaluacion.read',
+          'description' => 'Ver tipo de evaluacion'
+        ],[
+          'name'=> 'tipometodologia.create',
+          'slug' => 'tipometodologia.create',
+          'description' => 'Crear tipo de metodología'
+        ],[
+          'name'=> 'tipometodologia.update',
+          'slug' => 'tipometodologia.update',
+          'description' => 'Actualizar tipo de metodología'
+        ],[
+          'name'=> 'tipometodologia.delete',
+          'slug' => 'tipometodologia.delete',
+          'description' => 'Eliminar tipo de metodología'
+        ],[
+          'name'=> 'tipometodologia.read',
+          'slug' => 'tipometodologia.read',
+          'description' => 'Ver tipo de metodología'
+        ],[
+          'name'=> 'asignarpesos.create',
+          'slug' => 'asignarpesos.create',
+          'description' => 'Crear asignarpesos'
+        ],[
+          'name'=> 'asignarpesos.update',
+          'slug' => 'habilidades.update',
+          'description' => 'Actualizar asignarpesos'
+        ],[
+          'name'=> 'asignarpesos.delete',
+          'slug' => 'asignarpesos.delete',
+          'description' => 'Eliminar asignarpesos'
+        ],[
+          'name'=> 'asignarpesos.read',
+          'slug' => 'asignarpesos.read',
+          'description' => 'Ver asignarpesos'
         ],
       ]);
 
